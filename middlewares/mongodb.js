c0b0503e2e2bb407fe1447443da5f842d02397ff
philo-shoby <shoby.philo@gmail.com>
2023-08-25 13:24:03 +0530

    Security features

--- middlewares/mongodb.js
+++ middlewares/mongodb.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require("mongodb");
+const uri = require('../environment').URI;
 
 //MongoDB setup
-const uri = "mongodb+srv://lisalenoy:%40lisalenoy98@tourtraveldb.jnpkfge.mongodb.net/?retryWrites=true&w=majority";
+
 const mongoDb = new MongoClient(uri, { useNewUrlParser: true });
 
 module.exports = mongoDb;
